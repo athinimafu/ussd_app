e0ea236dfc0bbfdebdff973057c820eb3e3fcbbd
Nyasha <23294558@sun.ac.za>
2022-09-17 16:21:40 +0200

    Setup

---
+++ backend/africasTalking/business_func.py
@@ -0,0 +1,13 @@
+from flask import blueprints
+
+business_blueprint = blueprints.Blueprint(__name__, 'business')
+
+
+@business_blueprint.route('/deposit', 'GET')
+def deposit_view():
+    return "Deposit View: Some form will go here"
+
+
+@business_blueprint.route('/deposit', 'POST')
+def deposit():
+    return "Depositing to users"
--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,31 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
+from flask import request
+import africastalking
+
+load_dotenv()
+
+AT_SANDBOX = os.environ.get("AT_SANDBOX")
+AT_KEY = os.environ.get("AT_KEY")
+africastalking.initialize(AT_SANDBOX, AT_KEY)
 
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET"])
+def home():
+    return "hello world"
+
+
+@app.route('/callback', methods=["GET", "POST"])
+def callback():
+    response = ""
+    print(request.values)
+    response += "CON Hello World"
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
