8eca2b2b3a3e4411b6526602cb95af0b0461ee08
athinimafu <zanoxstoner@gmail.com>
2022-09-17 18:49:15 +0200

    checking if all user properties have been added

--- backend/africasTalking/user_func.py
+++ backend/africasTalking/user_func.py
@@ -1,5 +1,16 @@
 
+
 def user_registration(properties):
+    #properties : [ name, pin, confirmPin, phoneNumber ,ID ]
+    if len(properties) == 1:
+        return "please insert your full name"
+    elif len(properties) == 2:
+        return "please insert pin"
+    elif len(properties) == 3:
+        return "please insert confirmation of pin"
+    elif len(properties) == 4:
+        return "please insert your id"
+    name,_id,pin,confirmPin,phoneNumber = properties
     #TODO: insert into database then return SUCCESS or FAILURE
     return
 
