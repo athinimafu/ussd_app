8a767ec9aae832aec20020e11ba0f78da93c8ee6
Nyasha <23294558@sun.ac.za>
2022-09-18 04:31:23 +0200

    changes

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,6 @@ def home():
 
 @app.route('/callback', methods=["GET", "POST"])
 def callback():
-    User.delete("0660277901")
-    Transaction.get("0H3pzryAVNct0nbFfn68")
     user_phone_number = request.values.get("phoneNumber")
     session_code = request.values.get("sessionCode")
     serviceId = request.values.get("serviceCode")
@@ -27,67 +25,81 @@ def callback():
     registered = False
     response = ""
     name = "*"
-    
-    if (text == ""):
+
+    if text == "":
         response += "CON welcome to cash money \n"
         response += "1. login\n"
         response += "2. register\n"
         response += "3. exit"
         return response
-    text_array = text.split('*')
-    if (text == '1'):
+    if text == '1':
         response = ""
         response += "CON enter pin"
         return response
-    if request.values.get("text").startswith('1*'):
-        
-        print(request.values.get("text"))
-        return handle_login(request)
-    if (request.values.get("text").startswith('2')):
-        return handle_reg(request)
-    #return response
 
-def handle_login(request):
+    text_array = text.split("*")
+    if len(text_array) >= 2 and text_array[0] == "1":
+        print("HANDLE LOGIN")
+        user_pin = text_array[1]
+        user = User.get(user_phone_number)
+        if user:
+            if User.verify_pin(user["pin"], user_pin):
+                # TODO SET SESSION
+                return handle_logged_in_user(request, user_phone_number)
+            else:
+                return "END Incorrect pin."
+        else:
+            return "END Account does not exist. Please register first."
+    if len(text_array) == 2 and text_array[0] == "2":
+        return handle_reg(request, user_phone_number)
+    return "END Invalid option selected"
+
+
+def handle_logged_in_user(request, phone_num):
     text_array = request.values.get('text').split('*')
+    print(text_array)
     if len(text_array) == 2:
-        if User.get(text_array[1]):
-            response = ""
-            response += "CON 1. send money\n"
-            response += "2. view balance\n"
-            return response
-    else:
         response = ""
-        response += "END account does not exist please register first\n"
+        response += "CON 1. send money\n"
+        response += "2. view balance\n"
         return response
-    if request.values.get("text").endswith('1'):
-        print("ends with 1 ==sending money")
-        return handle_moneysend(request)
-    if request.values.get("text").startswith('1') and  len(text_array) == 4:
+    if text_array[0] == "1" and text_array[2] == "1" and len(text_array) >= 3:
+        return handle_money_send(request, phone_num)
+    if text_array[0] == "1" and text_array[2] == "2" and len(text_array) == 3:
         response = ""
-        response += "END money sent"
-        return response
-    text_array = request.values.get('text').split('*')
-    print(len(text_array),"length at your balance is")
-    if request.values.get("text").endswith('2'):
-        response = ""
-        response += "END your balance is =="
+        user = User.get(phone_num)
+        user_balance = user["balance"]
+        response += f"END Your balance is R{user_balance}"
         return response
 
-def handle_moneysend(request):
+
+def handle_money_send(request, sender_phone_num):
     text_array = request.values.get('text').split('*')
-    print(len(text_array), "--------")
-    if request.values.get("text").startswith('1') and  len(text_array) == 3:
-        print("********")
-        print(request.values.get("text"))
+    print("SSS",text_array)
+    if text_array[0] == "1" and text_array[2] == "1" and len(text_array) == 3:
         response = ""
-        response += " CON Enter amount"
+        response += "CON Enter recipient's phone number"
         return response
-    if request.values.get("text").startswith('1') and  len(text_array) == 4:
+    if text_array[0] == "1" and len(text_array) == 4:
         response = ""
-        response += "END money sent"
+        response += "CON Enter amount"
+        return response
+    if text_array[0] == "1" and len(text_array) == 5:
+        response = ""
+        recipient_phone = text_array[3]
+        amount = text_array[4]
+        user = User.get(sender_phone_num)
+        print("HERE")
+        if user:
+            if int(user["balance"]) >= int(amount):
+                User.transfer(sender_phone_num, recipient_phone, amount)
+                response += "END money sent"
+            else:
+                response += "END Insufficient funds"
         return response
 
-def handle_reg(request):
+
+def handle_reg(request, phone_num):
     if (request.values.get("text") == '2'):
         response = ""
         response += "CON Enter Name\n"
@@ -99,30 +111,29 @@ def handle_reg(request):
         response = ""
         response += "CON Enter Govid\n"
         return response
-    if  request.values.get("text").count('*') == 2:
+    if request.values.get("text").count('*') == 2:
         govid = request.values.get("text")
         print(govid)
         response = ""
         response += "CON Enter Pin\n"
         return response
-    if  request.values.get("text").count('*') == 3:
+    if request.values.get("text").count('*') == 3:
         pin = request.values.get("text")
         response = ""
         response += "CON Reenter Pin\n"
         print(request.values.get("text"))
         return response
-    if  request.values.get("text").count('*') == 4:
+    if request.values.get("text").count('*') == 4:
         user_array = request.values.get('text').split('*')
         uname = user_array[1]
         rsaid = user_array[2]
         pin = user_array[3]
-        phone = request.values.get("phoneNumber")
-        User.create(phone_num=phone, name=uname, id=rsaid, pin=pin)
+        User.create(phone_num, name=uname, id=rsaid, pin=pin)
         response = ""
         response += "END Fully registered\n"
         print(request.values.get("text"))
         return response
-    
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,8 @@
-from firebase_admin import credentials, firestore, initialize_app, auth
+from datetime import datetime
+
+from firebase_admin import credentials, firestore, initialize_app
 from google.cloud import firestore as f_store
+from google.cloud.firestore_v1 import DocumentReference
 
 from utils import hash_pin
 
@@ -15,7 +18,7 @@ transaction_ref = db.collection('transactions')
 
 class User:
     @staticmethod
-    def query_user_document(phone_num):
+    def query_user_document(phone_num) -> DocumentReference:
         return users.document(phone_num)
 
     @staticmethod
@@ -24,17 +27,17 @@ class User:
         user_data = {
             "name": name,
             "id": id,
-            "pin": hashed_pin
+            "pin": hashed_pin,
+            "balance": 0
         }
         user = User.query_user_document(phone_num)
-        user_obj = user.get()
-        if user_obj.to_dict() is None:
+        if not user.get().exists:
             user.set(user_data)
 
     @staticmethod
     def get(phone_num):
         user = User.query_user_document(phone_num).get()
-        if user:
+        if user.exists:
             return user.to_dict()
         else:
             return None
@@ -42,34 +45,30 @@ class User:
     @staticmethod
     def change_pin(phone_num, new_pin):
         queried_user = User.query_user_document(phone_num)
-        if queried_user:
+        if queried_user.get().exists:
             new_pin_hash = hash_pin(new_pin)
             queried_user.update({"pin": new_pin_hash})
             return True
         return False
 
     @staticmethod
-
-    def transfer(sender_num, receiver_num, balance):
+    def transfer(sender_num, receiver_num, amount):
         sender = User.query_user_document(sender_num)
         receiver = User.query_user_document(receiver_num)
-        if sender and receiver:
+        if sender.get().exists and receiver.get().exists:
             sender_dict = sender.get().to_dict()
-            if sender_dict["balance"] >= balance:
-                sender.update({u"balance": balance})
-                receiver.update({u"balance": f_store.Increment(balance)})
+            if sender_dict["balance"] >= amount:
+                sender.update({u"balance": f_store.Increment(-amount)})
+                receiver.update({u"balance": f_store.Increment(amount)})
+                Transaction.create(sender, receiver, amount)
 
     @staticmethod
     def get_balance(phone_num):
         user = User.query_user_document(phone_num)
-        return user['phone_num'] if user else None
-
-    @staticmethod
-    def delete(phone_num):
-        user = User.query_user_document(phone_num)
-        if user:
-            user.delete()
-        return
+        if user.get().exists:
+            return user.get().to_dict()['balance']
+        else:
+            return None
 
     @staticmethod
     def get_pin(phone_num):
@@ -84,30 +83,57 @@ class User:
             return True
         return False
 
+    @staticmethod
+    def delete(phone_num):
+        user = User.query_user_document(phone_num)
+        if user.get().exists:
+            user.delete()
+            return True
+        return False
+
 
 class Transaction:
     @staticmethod
-    def query_trans_document(transac_id):
+    def query_transac_document(transac_id) -> DocumentReference:
         return transaction_ref.document(transac_id)
-    
+
+    @staticmethod
+    def create(sender: DocumentReference, receiver: DocumentReference, amount):
+        transac_data = {
+            "amount": amount,
+            "sender": sender.id,
+            "receiver": receiver.id,
+            "date": datetime.utcnow()
+        }
+        transaction_ref.add(transac_data)
+
     @staticmethod
     def get(transac_id):
-        trans = Transaction.query_trans_document(transac_id)
-        trans_dict = trans.get().to_dict()
-        return trans_dict
+        transac = Transaction.query_transac_document(transac_id)
+        if transac.get().exists:
+            return transac.get().to_dict()
+        return None
 
 
 class Business(User):
-    @staticmethod
-    def get(phone_num):
-        business = User.get(phone_num)
-        if business:
-            return business.to_dict()
-        return None
 
     @staticmethod
-    def deposit(recipient, amount):
-        user = User.get(recipient)
-        if user:
-            user.update({u"balance":user["amount"]+amount})
-        return
+    def query_user_document(phone_num) -> DocumentReference:
+        return business_ref.document(phone_num)
+
+    @staticmethod
+    def get(phone_num):
+        business = Business.query_user_document(phone_num)
+        if business.get().exists:
+            return business.get().to_dict()
+        return None
+
+    # @staticmethod
+    # def deposit(receiver_num, amount):
+    #     receiver = User.query_user_document(receiver_num)
+    #     if receiver.get().exists:
+    #         receiver_dict = receiver.get().to_dict()
+    #         if receiver_dict["balance"] >= amount:
+    #             receiver.update({u"balance": f_store.Increment(-amount)})
+    #             receiver.update({u"balance": f_store.Increment(amount)})
+    #             Transaction.create(sender, receiver, amount)
